8b103f2fc76e11cd9577bfce9ab57d348f1b8a80
Psammy <ampadupsamuel1@gmail.com>
2025-03-19 23:51:37 +0000

    add styles

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './style.css';
 const Counter = () => {
+  const [count, setCount] = useState();
   return <div>Counter</div>;
 };
 
