38941640deab599eb8a0fea76ceb17ff1595ee0b
Psammy <ampadupsamuel1@gmail.com>
2025-03-20 11:04:41 +0000

    Add files

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,8 +1,25 @@
 import React, { useState } from 'react';
-import './style.css';
+import './counter.css';
 const Counter = () => {
-  const [count, setCount] = useState();
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  return (
+    <div className="container">
+      <div>
+        <h1 className="number">{count} </h1>
+      </div>
+
+      <div className="btns-container">
+        <button onClick={increment} className="increment">
+          +
+        </button>
+        <button onClick={decrement} className="decrement">
+          -
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
---
+++ src/Meal.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import axios from 'axios';
+import './meal.css';
+const Meal = () => {
+  return <div>Meal</div>;
+};
+
+export default Meal;
