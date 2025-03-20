9d1c6d5f3c03bdcf0fb47db1f8d69c3393da61db
Psammy <ampadupsamuel1@gmail.com>
2025-03-20 14:52:37 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import Meal from './Meal API project/Meal';
 import Meal2 from './Meal API project/Meal Api/Meal2';
 
 const App = () => {
   return (
     <div>
-      <Meal2 />
+      <Meal />
     </div>
   );
 };
