967a7fe51151aa7900ba79a15c0991be62a6b23f
Psammy <ampadupsamuel1@gmail.com>
2025-03-20 21:45:36 +0000

    Add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Calculator from './Calculator App/Calculator';
+import Toggletheme from './Toggle Theme/Toggletheme';
 
 const App = () => {
   return (
     <div>
-      <Calculator />
+      <Toggletheme />
     </div>
   );
 };
---
+++ src/Toggle Theme/Toggletheme.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Toggletheme = () => {
+  return <div>Toggletheme</div>;
+};
+
+export default Toggletheme;
