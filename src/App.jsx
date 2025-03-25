c63e64edfae08be0493ffb069255b54a5ce44323
Psammy <ampadupsamuel1@gmail.com>
2025-03-25 15:17:19 +0000

    Add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React, { useId } from 'react';
 import { accordionData } from './Accordion project/utils/content';
 import Accordion from './Accordion project/Accordion';
+import Form from './Form validation/Form';
 
 const App = () => {
   const id = useId();
   return (
     <>
-      <div className="accordion">
+      {/* <div className="accordion">
         {accordionData.map(({ title, content }) => (
           <Accordion title={title} content={content} />
         ))}
-      </div>
+      </div> */}
+      <Form />
     </>
   );
 };
