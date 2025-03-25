e20ddcc09bc9e1d074dbd5370261e9d7c062fc4d
Psammy <ampadupsamuel1@gmail.com>
2025-03-25 01:35:38 +0000

    Update

--- src/Search bar/HiddenSearchBar.jsx
+++ src/Search bar/HiddenSearchBar.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
 const HiddenSearchBar = () => {
-  return <div>HiddenSearchBar</div>;
+  const [showInput, setShowInput] = useState(false);
+  const [bgColor, setBgColor] = useState('white');
+
+  const handleClick = (e) => {
+    setBgColor(showInput ? 'white' : 'red');
+
+    setShowInput(!showInput);
+  };
+  return (
+    <section
+      onClick={handleClick}
+      style={{ backgroundColor: bgColor }}
+      className="container"
+    >
+      {showInput ? (
+        <input type="text" placeholder="Enter text..." />
+      ) : (
+        <FaSearch onClick={() => setShowInput(true)} />
+      )}
+    </section>
+  );
 };
 
 export default HiddenSearchBar;
