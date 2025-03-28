a3e3e73236de706dce674af81d325590034d3c36
Psammy <ampadupsamuel1@gmail.com>
2025-03-28 01:34:05 +0000

    Update

--- src/porfolio shi/Home.jsx
+++ src/porfolio shi/Home.jsx
@@ -4,16 +4,19 @@ import './home.css';
 import './styles.css';
 const Home = () => {
   return (
-    <section className="homepage">
-      <div className="image">
-        <img src={Matriculation} alt="" />
-      </div>
-      <div className="content">
-        <div className="greeting">
-          Hi, I'm <span>Psammy</span>{' '}
+    <main>
+      <section className="homepage">
+        <div className="content">
+          <div className="greeting">
+            Hi, I'm <span className="name">Psammy</span>
+          </div>
+          <div className="role">Full Stack Developer</div>
         </div>
-      </div>
-    </section>
+        <div className="image">
+          <img src={Matriculation} alt="" />
+        </div>
+      </section>
+    </main>
   );
 };
 
