498373322e5188cef45f09540cd15cf7a618e7f0
Psammy <ampadupsamuel1@gmail.com>
2025-03-28 02:00:13 +0000

    Update

--- src/porfolio shi/Home.jsx
+++ src/porfolio shi/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Matriculation from '../assets/Matriculation.jpg';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import { FaGithub } from 'react-icons/fa';
 import './home.css';
 import './styles.css';
 const Home = () => {
@@ -11,6 +14,20 @@ const Home = () => {
             Hi, I'm <span className="name">Psammy</span>
           </div>
           <div className="role">Full Stack Developer</div>
+          <div className="refer">
+            <button className="more-btn">More about me</button>
+            <div className="social-links">
+              <span className="link">
+                <FaLinkedin />
+              </span>
+              <span className="link">
+                <FaGithub />
+              </span>
+              <span className="link">
+                <FaXTwitter />
+              </span>
+            </div>
+          </div>
         </div>
         <div className="image">
           <img src={Matriculation} alt="" />
