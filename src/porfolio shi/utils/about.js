6c6b9c4d5c3d00f52de1c61457588dd29320f2cd
Psammy <ampadupsamuel1@gmail.com>
2025-03-29 19:31:58 +0000

    Refactor

--- src/porfolio shi/utils/about.js
+++ src/porfolio shi/utils/about.js
@@ -2,7 +2,7 @@ const desc = [{
   title: "Who is Samuel?",
   essay:"Samuel is a second-year Computer Science student at the University of Ghana. He is interested in developing practical solutions to real-world problems through programming. His goal is to become a full-stack developer, and he has already started working toward this objective.With consistent effort and dedication, Samuel is making steady progress in his studies and career development. He is focused on building a strong foundation in programming and is excited about the opportunities ahead."
 }, {
-  title: "why",
-  essay:"blah blah blah"
+  title: "How experienced am I?",
+  essay:"With a strong foundation in computer science and ongoing experience in software development, I've had the opportunity to work on a variety of personal projects, from web applications to mobile apps. My expertise includes programming languages like JavaScript, which I use to deliver high-quality software solutions."
   }]
 export default desc;
