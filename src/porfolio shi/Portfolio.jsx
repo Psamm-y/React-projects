affffbbe5173ce86b8586424b2e7a7922dd2df3b
Psammy <ampadupsamuel1@gmail.com>
2025-03-30 18:29:33 +0000

    Update

---
+++ src/porfolio shi/Portfolio.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+
+const Portfolio = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <About />
+    </div>
+  );
+};
+
+export default Portfolio;
