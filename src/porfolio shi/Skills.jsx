588ec6b38ea99cb3c629ac0dd1f2df5084852e62
Psammy <ampadupsamuel1@gmail.com>
2025-04-04 18:17:48 +0000

    Skills sec

---
+++ src/porfolio shi/Skills.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FaTools } from 'react-icons/fa';
+const Skills = () => {
+  return (
+    <div className="skills-section">
+      <div className="section-head">
+        <h2>Skills</h2>
+      </div>
+      <div className="skills-grid">
+        <div className="technical">
+          <h3>Technical Skills</h3>
+        </div>
+        <div className="soft">
+          <h3>Soft skills</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
