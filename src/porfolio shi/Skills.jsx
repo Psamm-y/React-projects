3f4015c4f6dd31f259aa3ef75e39ac98add97c5e
Psammy <ampadupsamuel1@gmail.com>
2025-04-04 18:56:24 +0000

    Add file

--- src/porfolio shi/Skills.jsx
+++ src/porfolio shi/Skills.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FaTools } from 'react-icons/fa';
+import { FaTools, FaLightbulb, FaHtml5 } from 'react-icons/fa';
+import './skills.css';
 const Skills = () => {
   return (
     <div className="skills-section">
@@ -8,10 +9,22 @@ const Skills = () => {
       </div>
       <div className="skills-grid">
         <div className="technical">
-          <h3>Technical Skills</h3>
+          <div className="technical-header">
+            <FaTools className="icon" />
+            <h3>Technical Skills</h3>
+          </div>
+          <div className="skills-list">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </div>
         </div>
         <div className="soft">
-          <h3>Soft skills</h3>
+          <div className="soft-header">
+            <FaLightbulb className="icon" />
+            <h3>Soft skills</h3>
+          </div>
         </div>
       </div>
     </div>
