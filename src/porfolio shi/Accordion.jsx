83ef3b90796145318e5de8b8d539585d329ec96b
Psammy <ampadupsamuel1@gmail.com>
2025-04-06 21:22:49 +0000

    change accordion icon

--- src/porfolio shi/Accordion.jsx
+++ src/porfolio shi/Accordion.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useState } from 'react';
 import './about.css';
+import { FaChevronCircleDown, FaChevronCircleUp } from 'react-icons/fa';
 const Accordion = ({ title, essay }) => {
   const [expand, setExpand] = useState(false);
 
   return (
     <>
-      <div className="accordion-header">
+      <div className="accordion-header" onClick={() => setExpand(!expand)}>
         <div className="title">{title}</div>
-        <button onClick={() => setExpand(!expand)}>{expand ? '-' : '+'}</button>
+        <span>{expand ? <FaChevronCircleUp /> : <FaChevronCircleDown />}</span>
       </div>
       <div className="accordion-content">{expand ? essay : ''}</div>
     </>
