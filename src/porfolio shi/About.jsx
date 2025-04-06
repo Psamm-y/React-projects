bcbf1020bcafeeee47974a62ed0344e7548bbaf6
Psammy <ampadupsamuel1@gmail.com>
2025-04-06 22:15:07 +0000

    refactor

--- src/porfolio shi/About.jsx
+++ src/porfolio shi/About.jsx
@@ -2,7 +2,10 @@ import React, { useId, useState } from 'react';
 import './about.css';
 import Accordion from './Accordion';
 import desc from './utils/about';
-import image from '../assets/ambulare-picture.jpg';
+import image from '../assets/Psammy.jpg';
+import { FaCalendar, FaPhone } from 'react-icons/fa';
+import { FaLocationDot } from 'react-icons/fa6';
+import { MdEmail } from 'react-icons/md';
 const About = () => {
   const id = useId();
   return (
@@ -11,7 +14,41 @@ const About = () => {
         <h2>About Me</h2>
       </div>
       <div className="main-about">
-        <img src={image} alt="" />
+        <div className="about-image">
+          <img src={image} alt="" />
+        </div>
+        <div className="info-section">
+          <div className="header">
+            <h3>Samuel Amoako Ampadu</h3>
+            <p>Full stack developer</p>
+          </div>
+          <div className="more-info">
+            <p>
+              <span>
+                <FaCalendar className="icon" /> Birthday:
+              </span>
+              &nbsp; 11th August 2005
+            </p>
+            <p>
+              <span>
+                <FaLocationDot className="icon" /> Location:
+              </span>
+              &nbsp; Ofankor Barrier, Accra: Ghana
+            </p>
+            <p>
+              <span>
+                <FaPhone className="icon" /> Phone:
+              </span>
+              &nbsp;+233 537 522 104
+            </p>
+            <p>
+              <span>
+                <MdEmail className="icon" /> Email:
+              </span>
+              &nbsp;ampadupsamuel1@gmail.com
+            </p>
+          </div>
+        </div>
       </div>
       <div className="about">
         {desc.map(({ title, essay }) => (
