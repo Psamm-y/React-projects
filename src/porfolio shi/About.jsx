056f489a74f2fed3ffe5f51134193e23517aebcb
Psammy <ampadupsamuel1@gmail.com>
2025-04-06 21:40:48 +0000

    refactor

--- src/porfolio shi/About.jsx
+++ src/porfolio shi/About.jsx
@@ -2,12 +2,16 @@ import React, { useId, useState } from 'react';
 import './about.css';
 import Accordion from './Accordion';
 import desc from './utils/about';
-const About = ({ title, essay }) => {
+import image from '../assets/ambulare-picture.jpg';
+const About = () => {
   const id = useId();
   return (
     <section className="about-section" id="about">
       <div className="section-head">
         <h2>About Me</h2>
+      </div>
+      <div className="main-about">
+        <img src={image} alt="" />
       </div>
       <div className="about">
         {desc.map(({ title, essay }) => (
