80b7ba83531a1e16bd3f9f6e323b2b0f3f066e9d
Psammy <ampadupsamuel1@gmail.com>
2025-04-07 21:21:30 +0000

    Add files

---
+++ src/porfolio shi/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      <div className="section-head">
+        <h2>Projects</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
