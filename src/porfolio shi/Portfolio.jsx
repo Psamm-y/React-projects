72f9ca068e7122135bf89828b409fc02a7ef3f5f
Psammy <ampadupsamuel1@gmail.com>
2025-04-17 19:08:24 +0000

    refactor

--- src/porfolio shi/Portfolio.jsx
+++ src/porfolio shi/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Header from './Header';
 import Home from './Home';
 import About from './About';
@@ -9,26 +9,29 @@ import Skills from './Skills';
 import Projects from './Projects';
 import Contact from './Contact';
 const Portfolio = () => {
-  const [showButton, setShowButton] = useState(false);
+  // const [showButton, setShowButton] = useState(false);
+  const buttonRef = useRef('');
   useEffect(() => {
     document.title = 'Psammy Porfolio';
   }, []);
-
-  const handleScroll = () => {
-    if (window.scrollY > 1000) {
-      setShowButton(true);
-    } else {
-      setShowButton(false);
-    }
-  };
 
   const handleClick = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
   useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 600) {
+        // setShowButton(true);
+        buttonRef.current?.classList.add('show');
+      } else {
+        // setShowButton(false);
+        buttonRef.current?.classList.remove('show');
+      }
+    };
+
     window.addEventListener('scroll', handleScroll);
     return () => {
-      window.removeEventListener(scroll, handleScroll);
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
   const [active, setActive] = useState(false);
@@ -38,11 +41,17 @@ const Portfolio = () => {
       <Home />
       <About />
       <Skills />
-      {showButton && (
-        <button className="move-to-top" onClick={() => handleClick()}>
+
+      <button
+        ref={buttonRef}
+        className="move-to-top"
+        onClick={() => handleClick()}
+      >
+        <span>
           <GoMoveToTop className="icon" />
-        </button>
-      )}
+        </span>
+      </button>
+
       <Projects />
       <Contact />
     </div>
